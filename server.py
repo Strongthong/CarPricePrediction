bb75301496e1051268149a18ab8ae669043959d7
thejustinn <79741337+thejustinn@users.noreply.github.com>
2024-04-04 16:44:51 +0800

    fix server.py

--- server.py
+++ server.py
@@ -147,4 +147,4 @@ def predict():
     output = prediction[0]
     return jsonify(output)
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
